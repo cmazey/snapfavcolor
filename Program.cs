8b2552b467805c0bc3b0cebf4402738d47ed86d7
cmazey <Cmazey10@outlook.com>
2024-03-02 21:46:28 -0500

    PROJECT UPDATE/VIDEO UPLOADED/DONEEEE

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿using Spectre.Console;
 
-Console.WriteLine("Hello World!");
+Console.WriteLine("Press anything to start program...");
+Console.ReadKey();
+Console.Clear();
+
+Console.WriteLine("[SYS] Snapfavcolor : STARTED\n");
+Thread.Sleep(1000);
 
 var snapFavColor = new Table();
-
+Console.WriteLine();
 await AnsiConsole.Live(snapFavColor)
 .StartAsync(async ctx =>
 {
@@ -47,19 +52,19 @@ await AnsiConsole.Live(snapFavColor)
     ctx.Refresh();
     await Task.Delay(100);
 
-    snapFavColor.AddRow("[white]Anthony Fratini[/]", "[white]Alex Fratini[/]", "[white]Chance Robinson[/]", "[white]Rusto[/]", "[white]Luke Dotson[/]", "[white]Daniel Monahan[/]", "[white]Bradley Sellers[/]", "[white]Brandon Reed[/]"); // Row 2
+    snapFavColor.AddRow("[white]Anthony Fratini[/]", "[white]Alex Fratini[/]", "[white]Chance Robinson[/]", "[white]Rusto[/]", "[white]Luke Dotson[/]", "[white]Daniel Monahan[/]", "[rgb(255,182,193)]Bradley Sellers[/]", "[white]Jack Chalmers[/]"); // Row 2
     ctx.Refresh();
     await Task.Delay(100);
 
-    snapFavColor.AddRow("[white]Rashee MclaurinJr[/]", "[white]John Mazey[/]", "[white]James Cox[/]", "[white]Tyler Valentino[/]", "[white]Daniel Beck[/]", "[white]Anderson Scott[/]", "", "[white]Angel Hillsman[/]"); // Row 3
+    snapFavColor.AddRow("[white]Rashee MclaurinJr[/]", "[white]Braeden Barker[/]", "[white]James Cox[/]", "[white]Tyler Valentino[/]", "[rgb(78,81,128)]Daniel Beck[/]", "[white]Anderson Scott[/]", "", "[white]Angel Hillsman[/]"); // Row 3
     ctx.Refresh();
     await Task.Delay(100);
 
-    snapFavColor.AddRow("[white]Wondu Evans[/]", "[white]Bradley Sellers[/]", "[white]Eli Pfister[/]", "[white]Nick Zarlinga[/]", "[white]John Mazey[/]", "", "", "[white]Jack Chalmers[/]"); // Row 4
+    snapFavColor.AddRow("[white]Wondu Evans[/]", "[white]Scotty N.[/]", "[white]Eli Pfister[/]", "[white]Nick Zarlinga[/]", "[white]John Mazey[/]", "", "", ""); // Row 4
     ctx.Refresh();
     await Task.Delay(100);
 
-    snapFavColor.AddRow("[white]Nolan Meyer[/]", "[white]Ella Pfister[/]", "[white]Jostahn Kuehner[/]", "", "Sean D."); // Row 5
+    snapFavColor.AddRow("[white]Nolan Meyer[/]", "[white]Ella Pfister[/]", "[rgb(1,50,32)]Jostahn Kuehner[/]", "", "Sean D."); // Row 5
     ctx.Refresh();
     await Task.Delay(100);
 
@@ -83,15 +88,7 @@ await AnsiConsole.Live(snapFavColor)
     ctx.Refresh();
     await Task.Delay(100);
 
-    snapFavColor.AddRow("", "[white]Daniel Beck[/]"); // Row 11
-    ctx.Refresh();
-    await Task.Delay(100);
-
-    snapFavColor.AddRow("", "[white]Braeden Barker[/]"); // Row 12
-    ctx.Refresh();
-    await Task.Delay(100);
-
-    snapFavColor.AddRow("", "[white]Scotty N.[/]"); // Row 13
+    snapFavColor.AddRow("", ""); // Row 11 (Emptied)
     ctx.Refresh();
     await Task.Delay(100);
 
@@ -101,12 +98,13 @@ await AnsiConsole.Live(snapFavColor)
     await Task.Delay(1000);
 });
 
-AnsiConsole.Write(new BarChart()
-    .Width(60)
-    .Label("[yellow bold underline]Friends on Snap[/]")
-    .CenterLabel()
-    .AddItem("Total Friends", 56, Color.Yellow)
-    .AddItem("Boys", 48, Color.Blue1)
-    .AddItem("Girls", 8, Color.Pink1));
+// AnsiConsole.Write(new BarChart()
+//    .Width(60)
+//    .Label("[yellow bold underline]Friends on Snap[/]")
+//    .CenterLabel()
+//    .AddItem("Total Friends", 56, Color.Yellow)
+//    .AddItem("Boys", 48, Color.Blue1)
+//    .AddItem("Girls", 8, Color.Pink1));
 
+Console.WriteLine("\n[SYS] Snapfavcolor : ENDED");
 Console.ReadKey();
